9a58d4ae1eb7ca3508f56e26e55a8d0218ab7f8e
Eduardo Lucas <eduardolucas40@gmail.com>
2019-07-25 17:27:49 -0300

    ajustes no controle de versao

--- apps/funcionarios/models.py
+++ apps/funcionarios/models.py
@@ -1,9 +1,16 @@
 from django.db import models
 
+from django.contrib.auth.models import User
+from apps.departamentos.models import Departamento
+from apps.empresas.models import Empresa
+
 
 class Funcionario(models.Model):
     """Manutenção do Cadastro de Funcionários"""
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     nome = models.CharField(max_length=100, help_text='Nome do funcionário')
+    departamentos = models.ManyToManyField(Departamento)
+    empresa = models.ForeignKey(Empresa, models.PROTECT)
     
     def __str__(self):
         return self.nome
