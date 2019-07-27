31cfe7b164441401e3151d4186416464676553e8
Eduardo Lucas <eduardolucas40@gmail.com>
2019-07-27 17:32:26 -0300

    Criado o CRUD de Funcionarios

--- apps/funcionarios/models.py
+++ apps/funcionarios/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 
 from django.contrib.auth.models import User
+from django.urls import reverse
+
 from apps.departamentos.models import Departamento
 from apps.empresas.models import Empresa
 
@@ -14,3 +16,7 @@ class Funcionario(models.Model):
     
     def __str__(self):
         return self.nome
+    
+    @staticmethod
+    def get_absolute_url():
+        return reverse('funcionario_list')
--- apps/funcionarios/urls.py
+++ apps/funcionarios/urls.py
@@ -1,6 +1,14 @@
 from django.urls import path
+from apps.funcionarios.views import (
+    FuncionarioList,
+    FuncionarioEdit,
+    FuncionarioDelete,
+    FuncionarioCreate)
 
 urlpatterns = [
-
+    path('list', FuncionarioList.as_view(), name='funcionario_list'),
+    path('create', FuncionarioCreate.as_view(), name='funcionario_create'),
+    path('edit/<int:pk>', FuncionarioEdit.as_view(), name='funcionario_edit'),
+    path('delete/<int:pk>', FuncionarioDelete.as_view(), name='funcionario_delete'),
     
 ]
